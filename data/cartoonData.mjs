1b8b02b7777cef111215504b25d518f25f41dbae
dayane112 <71454308+dayane112@users.noreply.github.com>
2024-10-22 14:44:01 -0400

    Adding cartoon and changing show data

---
+++ data/cartoonData.mjs
@@ -0,0 +1,50 @@
+export const cartoons = [
+    {
+        name: "X-Men: The Animated Series",
+        genre: "Sci-fi",
+        year: 1992,
+        seasons: 5
+    },
+    {
+        name: "Johnny Bravo",
+        genre: "Sitcom",
+        year: 1997,
+        seasons: 6
+    },
+    {
+        name: "Dexter's Laboratory",
+        genre: "Sci-fi",
+        year: 1995,
+        seasons: 4
+    },
+    {
+        name: "The Powerpuff Girls",
+        genre: "Action",
+        year: 1998,
+        seasons: 6
+    },
+    {
+        name: "Arthur",
+        genre: "Sitcom",
+        year: 1996,
+        seasons: 25
+    },
+    {
+        name: "Hey Arnold!",
+        genre: "Drama",
+        year: 1996,
+        seasons: 5
+    },
+    {
+        name: "The Wild Thornberrys",
+        genre: "Nature",
+        year: 1998,
+        seasons: 5
+    },
+    {
+        name: "Courage the Cowardly Dog",
+        genre: "Horror",
+        year: 1999,
+        seasons: 4
+    },
+]
--- data/showsData.mjs
+++ data/showsData.mjs
@@ -1,50 +1,50 @@
 export const shows = [
     {
-        name: "X-Men: The Animated Series",
-        genre: "Sci-fi",
-        year: 1992,
-        seasons: 5
-    },
-    {
-        name: "Johnny Bravo",
+        name: "The Fresh Prince of Bel-Air",
         genre: "Sitcom",
-        year: 1997,
+        year: 1990,
         seasons: 6
     },
     {
-        name: "Dexter's Laboratory",
-        genre: "Sci-fi",
-        year: 1995,
-        seasons: 4
+        name: "Mr. Bean",
+        genre: "Comedy",
+        year: 1990,
+        seasons: 1
     },
     {
-        name: "The Powerpuff Girls",
-        genre: "Action",
-        year: 1998,
-        seasons: 6
+        name: "Friends",
+        genre: "Sitcom",
+        year: 1994,
+        seasons: 10
     },
     {
-        name: "Arthur",
+        name: "Sabrina the Teenage Witch",
         genre: "Sitcom",
         year: 1996,
-        seasons: 25
+        seasons: 7
     },
     {
-        name: "Hey Arnold!",
-        genre: "Drama",
+        name: "Everybody Loves Raymond",
+        genre: "Sitcom",
         year: 1996,
-        seasons: 5
+        seasons: 9
     },
     {
-        name: "The Wild Thornberrys",
-        genre: "Nature",
-        year: 1998,
-        seasons: 5
+        name: "Boy Meets World",
+        genre: "Sitcom",
+        year: 1993,
+        seasons: 7
     },
     {
-        name: "Courage the Cowardly Dog",
-        genre: "Horror",
-        year: 1999,
-        seasons: 4
+        name: "Full House",
+        genre: "Sitcom",
+        year: 1987,
+        seasons: 8
+    },
+    {
+        name: "Family Matters",
+        genre: "Sitcom",
+        year: 1989,
+        seasons: 9
     },
 ]
