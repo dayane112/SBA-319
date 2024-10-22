688a58e61e3e355ce69446f4f0ae39a2e9e418e2
dayane112 <71454308+dayane112@users.noreply.github.com>
2024-10-22 14:16:29 -0400

    Adding movies data

---
+++ data/moviesData.mjs
@@ -0,0 +1,56 @@
+export const movies = [
+    {
+        name: "The Addams Family",
+        genre: ["Comedy", "Horror"],
+        year: 1991,
+        minutes: 99
+    },
+    {
+        name: "Hocus Pocus",
+        genre: ["Family", "Comedy"],
+        year: 1993,
+        minutes: 96
+    },
+    {
+        name: "A Goofy Movie",
+        genre: ["Family", "Comedy"],
+        year: 1995,
+        minutes: 78
+    },
+    {
+        name: "Edward Scissorhands",
+        genre: ["Fantasy", "Romance"],
+        year: 1990,
+        minutes: 105
+    },
+    {
+        name: "Halloweentown",
+        genre: ["Family", "Comedy"],
+        year: 1998,
+        minutes: 84
+    },
+    {
+        name: "The Iron Giant",
+        genre: ["Family", "Sci-fi"],
+        year: 1999,
+        minutes: 85
+    },
+    {
+        name: "Hook",
+        genre: ["Family", "Adventure"],
+        year: 1991,
+        minutes: 136
+    },
+    {
+        name: "Don't Tell Mom the Babysitter's Dead",
+        genre: ["Dark comedy", "Comedy"],
+        year: 1991,
+        minutes: 102
+    },
+    {
+        name: "The Sandlot",
+        genre: ["Family", "Comedy"],
+        year: 1993,
+        minutes: 102
+    },
+]
